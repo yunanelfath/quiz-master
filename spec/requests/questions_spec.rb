e0da8608cf323c7dc3e620cef4361cbd0ee82b88
Yunan KF <yunanelfath@ymail.com>
2017-02-05 22:55:09 +0700

    add info debugging login on spec

--- spec/requests/questions_spec.rb
+++ spec/requests/questions_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "Questions", type: :request do
   before :each do
     @user = User.create(email: 'test@gmail.com',password: 'asdfasdf')
 
-    login_as @user
+    login_as @user #http://stackoverflow.com/questions/23859653/rails-devise-rspec-undefined-method-sign-in
   end
   describe "GET /questions" do
     it "works! (now write some real specs)" do
